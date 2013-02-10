106f8b59f15402b58798175263358fbe3cb6d6bb
OpenShift guest <chrishadley@gmail.com>
2013-02-10 10:31:50 -0500

    socket

--- tankfront.js
+++ tankfront.js
@@ -4,6 +4,14 @@ var app = express()
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
+var gameData = {};
+gameData.playerCount = 0;
+gameData.players = [];
+gameData.height = 600,
+gameData.width = 800,
+
+
+
 server.listen(process.env.PORT);
 
 app.get('/', function (req, res) {
@@ -27,12 +35,46 @@ app.get('/js/kinetic.js', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
+    var id = gameData.playerCount++;
+    gameData.players[id] = new Player(socket,id);
+    console.log("Player " + id + " has joined.");
+    
+    socket.emit('current_position', {x: gameData.players[id].x, y: gameData.players[id].y, rotation: gameData.players[id].rotation});
     socket.on('my other event', function (data) {
         console.log(data);
     });
 });
 
+function Player(socket, name){
+    this.socket = socket;
+	this.name = name;
+    this.x = Math.floor(Math.random() * gameData.width);
+    this.y = Math.floor(Math.random() * gameData.height);
+    this.rotation = Math.floor(Math.random() * 360);
+}
+
+//function Tank(x, y, speedX, speedY, rotation) {
+//    this.rotation = rotation;
+//    this.x = x;
+//    this.y = y;
+//    this.speedX = speedX;
+//    this.speedY = speedY;
+//    this.bullets = [];
+//    console.log("x=" + x + " y=" + y);
+//    
+//    this.image = new Kinetic.Image({
+//        x: x,
+//        y: y,
+//        crop: {width: 32, height: 32, x: convertTileCoords(15,3).x, y: convertTileCoords(15,3).y},
+//        width: 32,
+//        height: 32,
+//        offset: [16,16],
+//        image: tanksGame.images.tiles
+//    });
+//    
+//    tanksGame.tanksLayer.add(this.image);
+//    tanksGame.tanksLayer.draw();    
+//}
 //var express = require('express'),
 //    app = express();
 //
